5d6e4fcc2ded0b6ccbe6e892d39768fc09a92d38
Master Loopy Bananamuffin <epicsqueebo@gmail.com>
2015-03-04 09:29:32 -0500

    Changed ParkourGlobals to ParkourGlobals.py
    I accidentally saved it with no file extension. I fixed it and added the
    map names and versions that will be displayed.

---
+++ toontown/parkour/ParkourGlobals.py
@@ -0,0 +1,18 @@
+#The following is a work-in-progress for a parkour racing feature.
+#This first part is the qualification times for a parkour race. There are 4
+#map versions in parkour: Practice, Short, Normal, and Mega.
+SHORT_QUALIFY_TIME = 90
+NORMAL_QUALIFY_TIME = 240
+MEGA_QUALIFY_TIME = 360
+#The next piece is the map names. Remember: There are four map versions!
+ParkourTTC = "Urban Trolley Follies"
+ParkourDD = "Parkour Ahoy!"
+ParkourDG = "Hedge Maze Marathon"
+ParkourMML = "Orchestrated Parkour"
+ParkourTB = "Iceberg Toondra"
+ParkourDDL = "Sleepy Parkour"
+#This little portion of the code contains the map versions themselves. The
+#two versions, Short and Mega, will be displayed.
+Practice = "[Practice]"
+Short = "[Short]"
+Mega = "[Mega]"
